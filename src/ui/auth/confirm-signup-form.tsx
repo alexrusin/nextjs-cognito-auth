e2f29b765ddcf5add6596d0079448c6f192f2c90
Alex Rusin <alexrusin_2000@yahoo.com>
2024-04-04 09:53:12 -0700

    update form to resend verification code

--- src/ui/auth/confirm-signup-form.tsx
+++ src/ui/auth/confirm-signup-form.tsx
@@ -15,72 +15,70 @@ import SendVerificationCode from "./send-verification-code-form";
 export default function ConfirmSignUpForm() {
   const [errorMessage, dispatch] = useFormState(confirmSignUp, undefined);
   return (
-    <>
-      <form action={dispatch} className="space-y-3">
-        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-          <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-            Please confirm your account.
-          </h1>
-          <div className="w-full">
-            <div>
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                htmlFor="email"
-              >
-                Email
-              </label>
-              <div className="relative">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="Enter your email address"
-                  required
-                />
-                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-              </div>
-            </div>
-            <div className="mt-4">
-              <label
-                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                htmlFor="code"
-              >
-                Code
-              </label>
-              <div className="relative">
-                <input
-                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  id="code"
-                  type="text"
-                  name="code"
-                  placeholder="Enter code"
-                  required
-                  minLength={6}
-                />
-                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-              </div>
+    <form action={dispatch} className="space-y-3">
+      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+          Please confirm your account.
+        </h1>
+        <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+                required
+              />
+              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
-          <ConfirmButton />
-          <div className="flex h-8 items-end space-x-1">
-            <div
-              className="flex h-8 items-end space-x-1"
-              aria-live="polite"
-              aria-atomic="true"
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="code"
             >
-              {errorMessage && (
-                <>
-                  <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                  <p className="text-sm text-red-500">{errorMessage}</p>
-                </>
-              )}
+              Code
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="code"
+                type="text"
+                name="code"
+                placeholder="Enter code"
+                required
+                minLength={6}
+              />
+              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
-      </form>
-      <SendVerificationCode />
-    </>
+        <ConfirmButton />
+        <div className="flex h-8 items-end space-x-1">
+          <div
+            className="flex h-8 items-end space-x-1"
+            aria-live="polite"
+            aria-atomic="true"
+          >
+            {errorMessage && (
+              <>
+                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                <p className="text-sm text-red-500">{errorMessage}</p>
+              </>
+            )}
+          </div>
+        </div>
+        <SendVerificationCode />
+      </div>
+    </form>
   );
 }
 
--- src/ui/auth/send-verification-code-form.tsx
+++ src/ui/auth/send-verification-code-form.tsx
@@ -14,39 +14,34 @@ export default function SendVerificationCode() {
     message: "",
     errorMessage: "",
   });
+  const { pending } = useFormStatus();
   return (
-    <form action={dispatch}>
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <ResendButton />
-        <div className="flex h-8 items-end space-x-1">
-          <div
-            className="flex h-8 items-end space-x-1"
-            aria-live="polite"
-            aria-atomic="true"
-          >
-            {response?.errorMessage && (
-              <>
-                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                <p className="text-sm text-red-500">{response.errorMessage}</p>
-              </>
-            )}
-            {response?.message && (
-              <p className="text-sm text-green-500">{response.message}</p>
-            )}
-          </div>
+    <>
+      <Button
+        className="mt-4 w-full"
+        aria-disabled={pending}
+        formAction={dispatch}
+      >
+        Resend Verification Code{" "}
+        <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      </Button>
+      <div className="flex h-8 items-end space-x-1">
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {response?.errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{response.errorMessage}</p>
+            </>
+          )}
+          {response?.message && (
+            <p className="text-sm text-green-500">{response.message}</p>
+          )}
         </div>
       </div>
-    </form>
-  );
-}
-
-function ResendButton() {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button className="mt-4 w-full" aria-disabled={pending}>
-      Resend Verification Code{" "}
-      <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </Button>
+    </>
   );
 }
